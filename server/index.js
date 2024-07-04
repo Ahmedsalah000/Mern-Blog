f51b23d3e6019dd2fe6af031f7dc8e91008ceae2
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-07-05 01:34:30 +0300

    index again

--- server/index.js
+++ server/index.js
@@ -8,25 +8,20 @@ import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import cors from 'cors';
-import 'colors';
+import dbConnection from './config/database.js';
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then((conn) => {
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+dbConnection();
+
 
 const __dirname = path.resolve();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors());
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
