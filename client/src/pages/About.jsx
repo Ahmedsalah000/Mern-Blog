a497a659a82f91dffe58884ef931009f7c6d6d43
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-07-19 20:35:38 +0300

    update about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 export default function About() {
   return (
     <div className='min-h-screen flex items-center justify-center'>
@@ -7,9 +8,17 @@ export default function About() {
             About me 
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
-            <p>
-            a MERN stack developer actively looking for a job. You aim to enhance your skills in the MERN stack and seek detailed explanations for various aspects of development. You prefer utilizing the latest updates for libraries within the MERN stack and other programming languages.
-            </p>
+          <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Ahmed Salah.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">MERN Stack</span>.
+      </motion.h1>
 
 
           </div>
